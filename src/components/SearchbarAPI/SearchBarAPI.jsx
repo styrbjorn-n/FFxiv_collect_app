e667cb69c065e0ab26e39d4c8ec4853d32cdb042
Styrbjörn Nordberg <styris.n@gmail.com>
2023-04-12 11:13:19 +0200

    added server sorting

--- src/components/SearchbarAPI/SearchBarAPI.jsx
+++ src/components/SearchbarAPI/SearchBarAPI.jsx
@@ -1,30 +1,39 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
-import "./SearchBarAPI.css";
-import CharacterResults from "../CharacterResults/CharacterResults";
+import './SearchBarAPI.css';
+import CharacterResults from '../CharacterResults/CharacterResults';
 
 const SearchBar = (props) => {
   console.clear();
   // a debouncer should be changed out for a proper react hook
-  const [input, setIput] = useState("");
-  const [characters, setCharacters] = useState("");
+  const [input, setIput] = useState('');
+  const [characters, setCharacters] = useState('');
+  const [server, setServer] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (input !== "") {
-        getCharacterID(input);
+      if (input !== '') {
+        getCharacterID(input, server);
       }
     }, 500);
     return () => clearTimeout(timer);
   }, [input]);
 
-  const handelchange = (e) => {
+  const handelNameChange = (e) => {
     setIput(e.target.value);
   };
 
-  function getCharacterID(name) {
-    fetch("https://xivapi.com/character/search?name=" + name)
+  const handelServerChange = (e) => {
+    setServer(e.target.value);
+  };
+
+  function getCharacterID(name, server) {
+    let serverSearch = '';
+    if (server !== '' && server !== undefined) {
+      serverSearch = '&server=' + server;
+    }
+    fetch('https://xivapi.com/character/search?name=' + name + serverSearch)
       .then((Response) => Response.json())
       .then(function (data) {
         setCharacters(data.Results);
@@ -38,16 +47,23 @@ const SearchBar = (props) => {
           type="text"
           placeholder="Character name..."
           value={input}
-          onChange={handelchange}
+          onChange={handelNameChange}
         />
+        <select onChange={handelServerChange}>
+          <option value=""></option>
+          <option value="alpha">Alpha</option>
+          <option value="lich">Lich</option>
+          <option value="odin">Odin</option>
+          <option value="phoenix">Phoenix</option>
+          <option value="raiden">Raiden</option>
+          <option value="Shiva">Shiva</option>
+          <option value="twintania">Twintania</option>
+          <option value="zodiark">Zodiark</option>
+        </select>
       </div>
       <div>
         <ul>
-          <CharacterResults
-            characters={characters}
-            searchResultHook={props.searchResultHook}
-            setSearchResultHook={props.setSearchResultHook}
-          />
+          <CharacterResults characters={characters} />
         </ul>
       </div>
     </div>
